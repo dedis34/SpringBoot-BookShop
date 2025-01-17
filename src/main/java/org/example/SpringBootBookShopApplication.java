6a41a12ab3b7f96b0661b075d702899cf089a747
Artur <a_blyskal@wp.pl>
2025-01-17 18:24:29 +0100

    first commit

---
+++ src/main/java/org/example/SpringBootBookShopApplication.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class SpringBootBookShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootBookShopApplication.class, args);
+    }
+    @Bean
+    public CommandLineRunner commandLineRunner() {
+        return new CommandLineRunner() {
+            @Override
+            public void run(String... args) throws Exception {
+
+            }
+        };
+    }
+
+}
