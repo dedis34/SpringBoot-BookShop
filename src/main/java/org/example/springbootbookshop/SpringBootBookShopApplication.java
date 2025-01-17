f7f919392d204aa2c3bfc570df25c60348ad20f5
Artur <a_blyskal@wp.pl>
2025-01-17 12:54:19 +0100

    Initial commit

---
+++ src/main/java/org/example/springbootbookshop/SpringBootBookShopApplication.java
@@ -0,0 +1,13 @@
+package org.example.springbootbookshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBookShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootBookShopApplication.class, args);
+    }
+
+}
