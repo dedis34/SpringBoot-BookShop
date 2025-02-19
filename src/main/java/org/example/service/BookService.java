fd70cbb7f3031384d19d0e04f68c4c64f1165762
Artur <a_blyskal@wp.pl>
2025-02-19 18:58:53 +0100

    Spring Boot Portfolio Project — Part 2

--- src/main/java/org/example/service/BookService.java
+++ src/main/java/org/example/service/BookService.java
@@ -1,4 +1,11 @@
 package org.example.service;
 
+import org.example.dto.BookDto;
+import org.example.dto.CreateBookRequestDto;
+import java.util.List;
+
 public interface BookService {
+    BookDto create(CreateBookRequestDto createBookRequestDto);
+    List<BookDto> getAll();
+    BookDto getById(Long id);
 }
